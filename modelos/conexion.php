b2d1d5f65928476c44638328ae00f9aa3ed0487a
Alejoflorez27 <101139306+Alejoflorez27@users.noreply.github.com>
2024-06-17 23:17:02 -0500

    cambios para la conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -6,8 +6,8 @@ class Conexion{
 
 		//se crea un objeto de tipo PDO (en el cual se le indica el localhost ademas el nombre de la base de datos;usuario BD;contraseña BD)
 		$link = new PDO("mysql:host=localhost;dbname=pos",
-						"root",
-						"");
+						"alejo",
+						"Guiday624$");
 
 		//se le idica los simbolos a manejar
 		$link -> exec("set names utf8");
