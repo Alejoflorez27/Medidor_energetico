3f90de983d2481f531b6dbb6c5efa9250f8364e1
Alejoflorez27 <101139306+Alejoflorez27@users.noreply.github.com>
2024-06-19 00:14:13 -0500

    cambios de conexion

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -3,11 +3,12 @@
 class Conexion{
 
 	static public function conectar(){
-
+		//conexion aws
 		//se crea un objeto de tipo PDO (en el cual se le indica el localhost ademas el nombre de la base de datos;usuario BD;contraseña BD)
 		$link = new PDO("mysql:host=localhost;dbname=pos",
 						"alejo",
 						"Guiday624$");
+		//conexion local con xampp
 		/*$link = new PDO("mysql:host=localhost;dbname=pos",
 		"root",
 		"");*/
